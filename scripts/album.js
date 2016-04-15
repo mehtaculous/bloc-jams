9061e18257d25bede367291674445315f93415bd
Sahil Mehta <samehta91@gmail.com>
2016-04-15 15:26:18 -0700

    Implement next and previous buttons on player bar

--- scripts/album.js
+++ scripts/album.js
@@ -1,53 +1,3 @@
-var albumAcidRap = {
-  title: 'Acid Rap',
-  artist: 'Chance The Rapper',
-  label: 'Independent',
-  year: '2013',
-  albumArtUrl: 'assets/images/album_covers/acid.jpg',
-  songs: [
-    { title: 'Good Ass Intro (feat. BJ The Chicago Kid, Lili K & Kiara Lanier)', duration: '3:59' },
-    { title: 'Pusha Man (feat. Nate Fox & Lili K)', duration: '7:24' },
-    { title: 'Cocoa Butter Kisses (feat. Vic Mensa & Twista)', duration: '5:07' },
-    { title: 'Juice', duration: '3:35'},
-    { title: 'Lost (feat. Noname Gypsy)', duration: '3:05'},
-    { title: 'Everybody\'s Something (feat. Saba & BJ The Chicago Kid)', duration: '4:37'},
-    { title: 'Interlude (That\'s Love)', duration: '2:30'},
-    { title: 'Favorite Song (feat. Childish Gambino)', duration: '3:05'},
-    { title: 'NaNa (feat. Action Bronson)', duration: '3:21'},
-    { title: 'Smoke Again (feat. Ab-Soul)', duration: '4:33'},
-    { title: 'Acid Rain', duration: '3:36'},
-    { title: 'Chain Smoker', duration: '3:31'},
-    { title: 'Everything\'s Good (Good Ass Outro)', duration: '5:33'}
-  ]
-};
-
-var albumControlSystem = {
-  title: 'Control System',
-  artist: 'Ab-Soul',
-  label: 'Top Dawg Entertainment',
-  year: '2012',
-  albumArtUrl: 'assets/images/album_covers/control.jpg',
-  songs: [
-    { title: 'Soulo Ho3 (feat. Jhené Aiko)', duration: '3:57' },
-    { title: 'Track Two', duration: '4:03' },
-    { title: 'Bohemian Grove', duration: '4:23' },
-    { title: 'Terrorist Threats (feat. Jhené Aiko & Danny Brown)', duration: '4:24'},
-    { title: 'Pineal Gland', duration: '3:52'},
-    { title: 'Double Standards', duration: '4:21'},
-    { title: 'Mixed Emotions', duration: '4:08'},
-    { title: 'SOPA (feat. Schoolboy Q)', duration: '4:09'},
-    { title: 'Lust Demons (feat. Jay Rock & BJ the Chicago Kid)', duration: '3:41'},
-    { title: 'ILLuminate (feat. Kendrick Lamar)', duration: '5:07'},
-    { title: 'A Rebellion (feat. Alori Joh)', duration: '3:48'},
-    { title: 'Showin\' Love', duration: '4:57'},
-    { title: 'Empathy (feat. Alori Joh & JaVonte)', duration: '3:01'},
-    { title: 'Nothing\'s Something', duration: '2:30'},
-    { title: 'Beautiful Death (feat. Punch & Ashtrobot)', duration: '4:30'},
-    { title: 'The Book of Soul', duration: '5:11'},
-    { title: 'Black Lip Bastard (Remix) (feat. Black Hippy)', duration: '5:49'}
-  ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">'
@@ -60,32 +10,36 @@ var createSongRow = function(songNumber, songName, songLength) {
   var $row = $(template);
 	
 	var clickHandler = function() {
-  	var songNumber = $(this).attr('data-song-number');
-		if (currentlyPlayingSong !== null) {
-			var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-			currentlyPlayingCell.html(currentlyPlayingSong);
+  	var songNumber = parseInt($(this).attr('data-song-number'));
+		if (currentlyPlayingSongNumber !== null) {
+			var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+			currentlyPlayingCell.html(currentlyPlayingSongNumber);
 		}
-		if (currentlyPlayingSong !== songNumber) {
+		if (currentlyPlayingSongNumber !== songNumber) {
 			$(this).html(pauseButtonTemplate);
-			currentlyPlayingSong = songNumber;
-		} else if (currentlyPlayingSong === songNumber) {
+			currentlyPlayingSongNumber = songNumber;
+			currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+			updatePlayerSongBar();
+		} else if (currentlyPlayingSongNumber === songNumber) {
 			$(this).html(playButtonTemplate);
-			currentlyPlayingSong = null;
+			$('.main-controls .play-pause').html(playerBarPlayButton);
+			currentlyPlayingSongNumber = null;
+			currentSongFromAlbum = null;
 		}
   };
 	
 	var onHover = function(event) {
 		var songNumberCell = $(this).find('.song-item-number');
-		var songNumber = songNumberCell.attr('data-song-number');
-		if (songNumber !== currentlyPlayingSong) {
+		var songNumber = parseInt(songNumberCell.attr('data-song-number'));
+		if (songNumber !== currentlyPlayingSongNumber) {
 			songNumberCell.html(playButtonTemplate);
 		}
 	};
 	
 	var offHover = function(event) {
 		var songNumberCell = $(this).find('.song-item-number');
-		var songNumber = songNumberCell.attr('data-song-number');
-		if (songNumber !== currentlyPlayingSong) {
+		var songNumber = parseInt(songNumberCell.attr('data-song-number'));
+		if (songNumber !== currentlyPlayingSongNumber) {
 			songNumberCell.html(songNumber);
 		}
 	};
@@ -96,6 +50,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
 var setCurrentAlbum = function(album) {
+	currentAlbum = album;
   var $albumTitle = $('.album-view-title');
 	var $albumArtist = $('.album-view-artist');
 	var $albumReleaseInfo = $('.album-view-release-info');
@@ -115,10 +70,86 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var nextSong = function() {
+	var getLastSongNumber = function(index) {
+		return index == 0 ? currentAlbum.songs.length : index;
+	};
+
+	var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+	currentSongIndex++;
+
+	if (currentSongIndex >= currentAlbum.songs.length) {
+		currentSongIndex = 0;
+	}
+
+	currentlyPlayingSongNumber = currentSongIndex + 1;
+	currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+	$('.currently-playing .song-name').text(currentSongFromAlbum.title);
+	$('.currently-playing .artist-name').text(currentAlbum.artist);
+	$('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
+	$('.main-controls .play-pause').html(playerBarPauseButton);
+
+	var lastSongNumber = getLastSongNumber(currentSongIndex);
+	var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+	var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+	$nextSongNumberCell.html(pauseButtonTemplate);
+	$lastSongNumberCell.html(lastSongNumber);
+}
+
+var previousSong = function() {
+	var getLastSongNumber = function(index) {
+		return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
+	};
+
+	var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+	currentSongIndex--;
+
+	if (currentSongIndex < 0) {
+		currentSongIndex = currentAlbum.songs.length - 1;
+	}
+
+	currentlyPlayingSongNumber = currentSongIndex + 1;
+	currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+	$('.currently-playing .song-name').text(currentSongFromAlbum.title);
+	$('.currently-playing .artist-name').text(currentAlbum.artist);
+	$('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
+	$('.main-controls .play-pause').html(playerBarPauseButton);
+
+	var lastSongNumber = getLastSongNumber(currentSongIndex);
+	var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+	var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+	$previousSongNumberCell.html(pauseButtonTemplate);
+	$lastSongNumberCell.html(lastSongNumber);
+};
+
+var trackIndex = function(album, song) {
+	return album.songs.indexOf(song);
+};
+
+var updatePlayerSongBar = function() {
+	$('.currently-playing .song-name').text(currentSongFromAlbum.title);
+	$('.currently-playing .artist-name').text(currentAlbum.artist);
+	$('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+	$('.main-controls .play-pause').html(playerBarPauseButton);
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentlyPlayingSong = null;
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauseButton = '<span class="ion-pause"></span>';
+var currentAlbum = null;
+var currentSongFromAlbum = null;
+var currentlyPlayingSongNumber = null;
+
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
 
 $(document).ready(function() {
   setCurrentAlbum(albumAcidRap);
+	$previousButton.click(previousSong);
+  $nextButton.click(nextSong);
 });
